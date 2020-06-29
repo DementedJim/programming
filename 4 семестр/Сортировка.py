09ac473bad83341cb7f06e75e8870410a151eeb7
Dmitry Ivanov <31902006+DementedJim@users.noreply.github.com>
2020-06-29 04:48:20 +0300

    Update Сортировка.py

--- 4 семестр/Сортировка.py
+++ 4 семестр/Сортировка.py
@@ -16,7 +16,20 @@ def insertion(data):
 		data[j + 1] = key
 	return data
 
+def parity(m):
+    a = []
+    b = []
+    for x in (m):
+        if x % 2 == 0:
+            a.append(x)
+        else:
+            b.append(x)
+    print("Четные элементы: ", a)
+    print("Нечетные элементы: ", b)
+
+
 a = list(np.random.randint(0, 10, 10))
 print(a)
 print(insertion (a))
 vstr(a)
+parity(insertion(a))
