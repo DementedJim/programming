98aec078198b8f7c0a5d1bbd7996621e97de27bf
Dmitry Ivanov <31902006+DementedJim@users.noreply.github.com>
2020-06-29 07:48:01 +0300

    Update Подсчет слов.py

--- 6 семестр/Подсчет слов.py
+++ 6 семестр/Подсчет слов.py
@@ -3,7 +3,7 @@ def wordcounter(s):
   words = s.split(' ')
   for i in words:
      if(i not in '!,.1234567890-+;[]{}'):
-       yield print("размер {0}-го слова {1}: {2}".format(c, i, len(i)))
+       yield print("Длина {0}-го слова {1}: {2}".format(c, i, len(i)))
        c+=1
 
 a = input("Введите текст: \t\n")
